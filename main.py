689c7892658dd978c3b00d8e5098a6a4700da4b2
evgenij <jenkz1@yandex.ru>
2023-04-15 17:02:23 +0300

    [chore] Добавлена логика парсинга страниц

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import asyncio
+import aiohttp
 from transliterate import translit
-import requests
 import socket
 
 
@@ -13,16 +14,33 @@ def check_internet_connection():
     return False
 
 
-def main(input_keywords):
+async def fetch(session, url):
+    async with session.get(url) as response:
+        if response.status == 200:
+            print(f'Парсим {url}')
+            return await response.text()
+        else:
+            print(f'Ошибка {response.status} на {url}')
+
+
+async def main(input_keywords, search_words_text='', stop_words_text=''):
     domain_url = 'https://telegra.ph/'
     search_keywords = translit(input_keywords.lower(), "ru", reversed=True)
 
     if check_internet_connection():
-        response = requests.get(f'{domain_url}{search_keywords}-01-01')
-        print(response.content)
+        tasks = []
+        async with aiohttp.ClientSession() as session:
+            for i in range(1, 1 + 1):
+                for j in range(1, 31 + 1):
+                    monday = str(i).zfill(2)
+                    day = str(j).zfill(2)
+                    url = f'{domain_url}{search_keywords}-{monday}-{day}'
+                    tasks.append(asyncio.create_task(fetch(session, url)))
+                    await asyncio.sleep(0.2)
+            responses = await asyncio.gather(*tasks)
+
+            print(len(responses))
 
 
 if __name__ == '__main__':
-    main('курсы')
-
-
+    asyncio.run(main('курсы'))
